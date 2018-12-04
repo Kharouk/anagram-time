52b40f67ec7c00e499285b283229c8d908a800c6
Alex Kharouk <akharoukpp@gmail.com>
2018-12-04 16:46:13 +0000

    ADD new Component, for chosenWord

---
+++ src/components/chosenWord.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+export default class ChosenWord extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      word: ""
+    };
+  }
+}
